589f715d3ef59a4d2ea8dbe1448a5da132155b25
wednesdayfin <raimovormisto86@gmail.com>
2024-04-17 17:41:32 +0300

    Update projekti.c

--- projekti.c
+++ projekti.c
@@ -7,70 +7,82 @@
 #include <BlynkSimpleWiFiNINA.h>
 
 char auth[] = BLYNK_AUTH_TOKEN;
-char ssid[] = "4G-Gateway-E6F8";
-char pass[] = "EE5NTMT4TGB";
+//char ssid[] = "4G-Gateway-E6F8";
+//char pass[] = "EE5NTMT4TGB";
+char ssid[] = "HevomPerse";
+char pass[] = "nowo3680";
+
 BlynkTimer timer;
 
 int value = 0;
+double analogValue = 0;
+double V_out = 0;
+double T_A = 0;
+double fahr = 0;
 
 void myTimerEvent() {
   Blynk.virtualWrite(V2, millis()/1000);
   analogValue = analogRead(A1);
   V_out = 3.3 * (double)analogValue / 4095;
   T_A = V_out / 0.010 - 273.15;
-  Blynk.virtualWriteBinary(V1, T_A);
-//Esimerkkisovellus: Jos lämpötila > 25 kirjoitetaan V1 1, muuten 0.
-
-if(T_A >= 25) {
-  Blynk.virtualWrite(V4, 1);
-  Blynk.virtualWrite(V5, 0);
-  Blynk.virtualWrite(V6, 0);
-  Blynk.virtualWrite(V7, 0);
-} 
-else if (25 > T_A >= 26) {
-  Blynk.virtualWrite(V5, 1);
-  Blynk.virtualWrite(V4, 0);
-  Blynk.virtualWrite(V6, 0);
-  Blynk.virtualWrite(V7, 0);
-}
-else if (26 > T_A >= 27) {
-  Blynk.virtualWrite(V6, 1);
-  Blynk.virtualWrite(V4, 0);
-  Blynk.virtualWrite(V5, 0);
-  Blynk.virtualWrite(V7, 0);
-}
-else {
-  Blynk.virtualWrite(V7, 1);
-  Blynk.virtualWrite(V4, 0);
-  Blynk.virtualWrite(V5, 0);
-  Blynk.virtualWrite(V6, 0);
-}
+  fahr = 1.8 * T_A + 32;
+  Blynk.virtualWrite(V1, T_A);
+  Blynk.virtualWrite(V4, fahr);
 }
 
-/*
-BLYNK_WRITE(V0) {
+BLYNK_WRITE(V1) {
   value = param.asInt();
-  if (value == 1) {
-   digitalWrite(5, 1);
-  }
   if (value == 0) {
-    digitalWrite(5, 0);
+    digitalWrite(0, 0);
+    digitalWrite(1, 0);
+    digitalWrite(2, 0);      
+  }
+  if (value == 1) {
+    digitalWrite(0, 1);
+    digitalWrite(1, 1);
+    digitalWrite(2, 1);
+    
   }
 }
-*/
 
 void setup() {
   Blynk.begin(auth,ssid,pass);
   timer.setInterval(1000L, myTimerEvent);
   analogReadResolution(12);
   pinMode(0, OUTPUT);
-  pinMode(3, INPUT_PULLDOWN);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   Blynk.run();
   timer.run();
-  switchStatus = digitalRead(3);
-  digitalWrite(0, switchStatus);
+  if (value == 1) {
+    digitalWrite(0, 1);
+    digitalWrite(1, 1);
+    digitalWrite(2, 1);      
+  }
+  else
+  {
+    if (T_A <= 25) {
+      digitalWrite(0, 0);
+      digitalWrite(1, 0);
+      digitalWrite(2, 0);
+      }
+      else if (25 < T_A && T_A <= 26) {
+      digitalWrite(0, 1);
+      digitalWrite(1, 0);
+      digitalWrite(2, 0);
+      }
+      else if (26 < T_A && T_A <= 27) {
+      digitalWrite(0, 1);
+      digitalWrite(1, 1);
+      digitalWrite(2, 0);
+      }
+      else {
+      digitalWrite(0, 1);
+      digitalWrite(1, 1);
+      digitalWrite(2, 1);
+    }
+  }
 }
